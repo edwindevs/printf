44de0c7fa03ea8fde64b64b7decee38e335a2e2d
edwindevs <edmbogo@outlook.com>
2022-05-23 00:42:39 +0000

    correcting a typo on customized_function.c

--- customized_function.c
+++ customized_function.c
@@ -2,14 +2,14 @@
 #include "main.h"
 
 /**
- *print_nonXter - Non printable characters
- *(0 < ASCII value < 32 >= 127) are printed this way:
- *\x, followed by ASCII code value in hexadecimal(uppercase
- *and always 2 characters
- *@l: va_list arguments from _printf
- *@f: pointer to the structargz that determines if a varg is 
- *passed to _printf
- *Return: number of char printed
+ * print_nonXter - Non printable characters
+ * (0 < ASCII value < 32 or >= 127) are
+ * printed this way: \x, followed by the ASCII code
+ * value in hexadecimal (upper case - always 2 characters)
+ * @l: va_list arguments from _printf
+ * @f: pointer to the struct argz that determines
+ * if a varg is passed to _printf
+ * Return: number of char printed
  */
 int print_nonXter(va_list l, argz_x *f)
 {
@@ -20,13 +20,14 @@ int print_nonXter(va_list l, argz_x *f)
 	(void)f;
 	if (!s)
 		return (_puts("(null)"));
+
 	for (i = 0; s[i]; i++)
 	{
 		if (s[i] > 0 && (s[i] < 32 || s[i] >= 127))
 		{
 			_puts("\\x");
 			count += 2;
-			ter = transform(s[i], 6, 0);
+			ter = transform(s[i], 16, 0);
 			if (!ter[1])
 				count += _putchar('0');
 			count += _puts(ter);
@@ -38,36 +39,37 @@ int print_nonXter(va_list l, argz_x *f)
 }
 
 /**
- *print_reverse - prints reverse of a string
- *@l: arguments from _printf
- *@f: pointer to the struct argz
- *Return: length of the printed string
- *
+ * print_reverse - prints a string in reverse
+ * @l: argument from _printf
+ * @f: pointer to the struct argz that determines
+ * if a varg is passed to _printf
+ * Return: length of the printed string
  */
-
 int print_reverse(va_list l, argz_x *f)
 {
-	int j, i = 0;
+	int i = 0, j;
 	char *s = va_arg(l, char *);
 
 	(void)f;
 	if (!s)
 		s = "(null)";
+
 	while (s[i])
 		i++;
-	for (j = 1 - 1; j >= 0; j--)
+
+	for (j = i - 1; j >= 0; j--)
 		_putchar(s[j]);
+
 	return (i);
 }
 
 /**
- *print_rot13 - prints s atring using rot13
- *@l: list of arguments
- *@f: pointerto the struct
- *
- *Return: length of the printed string
+ * print_rot13 - prints a string using rot13
+ * @l: list of arguments from _printf
+ * @f: pointer to the struct argz that determines
+ * if a varg is passed to _printf
+ * Return: length of the printed string
  */
-
 int print_rot13(va_list l, argz_x *f)
 {
 	int i, j;
@@ -89,15 +91,15 @@ int print_rot13(va_list l, argz_x *f)
 			}
 		}
 	}
-	return (j);
 
+	return (j);
 }
 
 /**
- *print_percent - prints a percent
- *@l: va_list arguments
- *@f: pointer to the struct
- *Return: number of characetr
+ * print_percent - prints a percent
+ * @l: va_list arguments from _printf
+ * @f: pointer to the struct argz in which we turn the argz on
+ * Return: number of char printed
  */
 int print_percent(va_list l, argz_x *f)
 {
@@ -105,3 +107,4 @@ int print_percent(va_list l, argz_x *f)
 	(void)f;
 	return (_putchar('%'));
 }
+
