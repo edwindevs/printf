5dc4b47d4b6c996ca745efa78116866edbcf968a
edwindevs <edmbogo@outlook.com>
2022-05-23 02:49:57 +0000

    mandatory and advanced tasks

--- address_function.c
+++ address_function.c
@@ -2,27 +2,26 @@
 #include "main.h"
 
 /**
- *print_address - prints address of input in hexa format
- *@l: va_list arguments from _printf
- *@f: pointer to the struct argz that determines if arg
- *is passes to _printf
- *Return: number of char printed
+ * print_address - prints address of input in hexa format
+ * @l: va_list arguments from _printf
+ * @f: pointer to the struct argz that determines
+ * if a varg is passed to _printf
+ * Return: number of char printed
  */
-
 int print_address(va_list l, argz_x *f)
 {
-	char *str;
+    char *str;
+    unsigned long int p = va_arg(l, unsigned long int);
 
-	unsigned long int p = va_arg(l, unsigned long int);
+    register int count = 0;
 
-	register int count = 0;
+    (void)f;
 
-	(void)f;
-
-	if (!p)
-		return (_puts("(nil)"));
-	str = transform(p, 16, 1);
-	count += _puts("0x");
-	count += _puts(str);
-	return (count);
+    if (!p)
+        return (_puts("(nil)"));
+    str = transform(p, 16, 1);
+    count += _puts("0x");
+    count += _puts(str);
+    return (count);
 }
+
--- base_function.c
+++ base_function.c
@@ -1,86 +1,85 @@
-
 #include "main.h"
-#include <stdarg.h>
+
 /**
- *print_hexaDec - prints a number in hexadecimal base
- *@l: va_list argumentsfrom _printf
- *@f: pointer to the struct argz that determines if a varg
- *is passed to _printf
- *Description: the function calls transform() which in turn
- *transformst the input number into the correct base
- *
+ * print_hexaDec - prints a number in hexadecimal base,
+ * in smallcaps
+ * @l: va_list arguments from _printf
+ * @f: pointer to the struct argz that determines
+ * if a varg is passed to _printf
+ * Description: the function calls transform() which in turns transforms the input
+ * number into the correct base and returns it as a string
  * Return: the number of char printed
  */
-
 int print_hexaDec(va_list l, argz_x *f)
 {
-	unsigned int num = va_arg(l, unsigned int);
-	char *str = transform(num, 16, 1);
-	int count = 0;
+    unsigned int num = va_arg(l, unsigned int);
+    char *str = transform(num, 16, 1);
+    int count = 0;
 
-	if (f->hTag == 1 && str[0] != '0')
-		count += _puts("0x");
-	count += _puts(str);
-	return (count);
+    if (f->hTag == 1 && str[0] != '0')
+        count += _puts("0x");
+    count += _puts(str);
+    return (count);
 }
 
 /**
- *print_hexaDec_big - print a number in hexa base in uppercase
- *@l: va_list arguments from _printf
- *@f: pointer to the structthat determines if arg is passed
- *Description: the function calls transform()
- *
- *Return: the number of char printed
+ * print_hexaDec_big - prints a number in hexadecimal base,
+ * in uppercase
+ * @l: va_list arguments from _printf
+ * @f: pointer to the struct that determines
+ * if a varg is passed to _printf
+ * Description: the function calls transform() which in turns transforms the input
+ * number into the correct base and returns it as a string
+ * Return: the number of char printed
  */
-
 int print_hexaDec_big(va_list l, argz_x *f)
 {
-	unsigned int num = va_arg(l, unsigned int);
-	char *str = transform(num, 16, 0);
-	int count = 0;
+    unsigned int num = va_arg(l, unsigned int);
+    char *str = transform(num, 16, 0);
+    int count = 0;
 
-	if (f->hTag == 1 && str[0] != '0')
-		count += _puts("0x");
-	count += _puts(str);
-	return (count);
+    if (f->hTag == 1 && str[0] != '0')
+        count += _puts("0X");
+    count += _puts(str);
+    return (count);
 }
 
 /**
- *print_bin - print a no in ase 2
- *@l: va_list
- *@f: pointer to the struct
- *Description: the unction calls transform() to transofrm
- *the input
- *
- *Return: the number of char printed
+ * print_bin - prints a number in base 2
+ * @l: va_list arguments from _printf
+ * @f: pointer to the struct that determines
+ * if a varg is passed to _printf
+ * Description: the function calls transform() which in turns transforms the input
+ * number into the correct base and returns it as a string
+ * Return: the number of char printed
  */
-
 int print_bin(va_list l, argz_x *f)
 {
-	unsigned int num = va_arg(l, unsigned int);
-	char *str = transform(num, 2, 0);
+    unsigned int num = va_arg(l, unsigned int);
+    char *str = transform(num, 2, 0);
 
-	(void)f;
-	return (_puts(str));
+    (void)f;
+    return (_puts(str));
 }
 
 /**
- *print_base - print in base 8
- *@l: va_list arg
- *@f: pointer to the structthat determines if a varg
- *is passed to int
- *Description: the function call transform()
- *Return: the number of char printed
+ * print_base - prints a number in base 8
+ * @l: va_list arguments from _printf
+ * @f: pointer to the struct that determines
+ * if a varg is passed to _printf
+ * Description: the function calls transform() which in turns transforms the input
+ * number into the correct base and returns it as a string
+ * Return: the number of char printed
  */
-
 int print_base(va_list l, argz_x *f)
 {
-	unsigned int num = va_arg(l, unsigned int);
-	char *str = transform(num, 8, 0);
-	int count = 0;
+    unsigned int num = va_arg(l, unsigned int);
+    char *str = transform(num, 8, 0);
+    int count = 0;
 
-	if (f->hTag == 1 && str[0] != '0')
-		count += _putchar('0');
-	count += _puts(str);
-	return (count);
+    if (f->hTag == 1 && str[0] != '0')
+        count += _putchar('0');
+    count += _puts(str);
+    return (count);
 }
+
